06f665580b2d5505a182f3d9e2700fd05bbd220a
Dhanush <dha3102003@gmail.com>
2024-08-03 15:10:30 +0530

    ENV Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import FileUpload from "./Utils/FileUpload";
+import FileUpload from "./Pages/Home/FileUpload";
 import Home from "./Pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
+    <Routes>
+      <Route path=""></Route>
+    </Routes>
       <Home />
       <FileUpload />
     </>
