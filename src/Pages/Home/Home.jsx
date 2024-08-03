2e188c78597d3a0ad2a0267456011c79c95130f5
Neelesh Thonse Rao <neeleshrao1@gmail.com>
2024-08-03 21:26:01 +0530

    USN upload one time done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect } from "react";
 import { useRef } from "react";
 import { firestore } from "../../firebase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, query, where, getDocs } from "firebase/firestore";
 import { useAuth } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
-  const messageRef = useRef();
-  const ref = collection(firestore, "messages");
-  const navigate=useNavigate();
-  const {logOut,user}=useAuth();
+  const usnRef = useRef();
+  const ref = collection(firestore, "usnmapping");
+  const navigate = useNavigate();
+  const { logOut, user } = useAuth();
 
   // useEffect(()=>{
   //   console.log("user",user)
@@ -19,31 +19,38 @@ function Home() {
 
   const handleSave = async (e) => {
     e.preventDefault();
-    console.log(messageRef.current.value);
+    console.log(usnRef.current.value);
+    const the_usn = usnRef.current.value;
+    const q = query(ref, where("user_id", "==", user.uid));
+    const querySnapshot = await getDocs(q);
+    if (querySnapshot.size == 0) {
+      let data = {
+        user_id: user.uid,
+        usn: usnRef.current.value,
+      };
 
-    let data = {
-      message: messageRef.current.value,
-    };
-
-    try {
-      addDoc(ref, data);
-    } catch (error) {
-      console.log(error);
+      try {
+        addDoc(ref, data);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      console.log("USN already entered!");
     }
   };
-  const handleLogout=async()=>{
-      try{
-        await logOut();
-      }catch(err){
-        console.log(err)
-      }
-  }
+  const handleLogout = async () => {
+    try {
+      await logOut();
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div>
       <h1>{user?.displayName}</h1>
       <form onSubmit={handleSave}>
-        <label>Enter Message</label>
-        <input type="text" ref={messageRef} />
+        <label>Enter USN</label>
+        <input type="text" ref={usnRef} />
         <button type="submit">Save</button>
       </form>
       <button onClick={handleLogout}>Logout</button>
