6a7bfc3b5816350079234cdac430d536f4ced9d5
S MOHAMMED ASHIQ <105161538+SMOHAMMEDASHIQ@users.noreply.github.com>
2024-09-03 23:42:54 +0530

    Updated Publication.jsx

--- src/Pages/Publication/Publication.jsx
+++ src/Pages/Publication/Publication.jsx
@@ -1,18 +1,24 @@
+
 import React from "react";
 import { useAuth } from "../../Context/AuthContext";
-import HNavbar from "../../Components/HNavbar";
 import VNavbar from "../../Components/VNavbar";
+import HNavbar from "../../Components/HNavbar";
 import Content from "./Content";
+import { InternProvider } from "../../Context/PublicationContext";
 
-export default function Publication() {
+function Publication() {
   const { logOut, user } = useAuth();
   return (
     <>
       <HNavbar />
-      <div className="flex absolute top-[100px] left-0 right-0 bottom-0">
-        <VNavbar option="Publication" />
-        <Content/>
+      <div className="flex absolute top-[100px] left-0 right-0 bottom-0 bg-gray-200 ">
+        <VNavbar option="Publication"/>
+        <InternProvider>
+          <Content />
+        </InternProvider>
       </div>
     </>
   );
 }
+
+export default Publication;
