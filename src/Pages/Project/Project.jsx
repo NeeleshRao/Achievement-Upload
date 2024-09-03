769519557d2bc7bda1711ca2dc1ed682c6229b84
S MOHAMMED ASHIQ <105161538+SMOHAMMEDASHIQ@users.noreply.github.com>
2024-09-03 23:40:31 +0530

    Updated Project.jsx

--- src/Pages/Project/Project.jsx
+++ src/Pages/Project/Project.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import HNavbar from '../../Components/HNavbar';
+
+import React from "react";
+import { useAuth } from "../../Context/AuthContext";
 import VNavbar from "../../Components/VNavbar";
-import Content from './Content';
-import { useAuth } from '../../Context/AuthContext';
+import HNavbar from "../../Components/HNavbar";
+import Content from "./Content";
+import { InternProvider } from "../../Context/ProjectContext";
+
 function Project() {
-    const { logOut, user } = useAuth();
-    return (
-      <>
-        <HNavbar />
-        <div className="flex absolute top-[100px] left-0 right-0 bottom-0">
-          <VNavbar option="Project" />
+  const { logOut, user } = useAuth();
+  return (
+    <>
+      <HNavbar />
+      <div className="flex absolute top-[100px] left-0 right-0 bottom-0 bg-gray-200 ">
+        <VNavbar option="Project"/>
+        <InternProvider>
           <Content />
-        </div>
-      </>
-    );
+        </InternProvider>
+      </div>
+    </>
+  );
 }
 
-export default Project
+export default Project;
