d6a672139777d65481a2656938b42f668ef912d0
S MOHAMMED ASHIQ <105161538+SMOHAMMEDASHIQ@users.noreply.github.com>
2024-09-03 23:46:36 +0530

    Uploaded sports , publication and project contexts

---
+++ src/context/PublicationContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useContext, useState } from "react";
+import React from "react";
+
+export const internContext=createContext();
+
+export function useIntern(){
+    return useContext(internContext);
+}
+
+export function InternProvider({children}){
+    const [openModel,setOpenModel]=useState(false);
+    const [interns, setInterns] = useState();
+    return (
+        <internContext.Provider value={{openModel,setOpenModel,interns,setInterns}}>
+            {children}
+        </internContext.Provider>
+    )
+}
