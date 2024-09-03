9fb07689111cd920611cb7f60d7fe807122f76de
S MOHAMMED ASHIQ <105161538+SMOHAMMEDASHIQ@users.noreply.github.com>
2024-09-03 23:42:26 +0530

    Updated Content.jsx

--- src/Pages/Publication/Content.jsx
+++ src/Pages/Publication/Content.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Bar from './Bar'
+import Model from './Model'
+import Outlay from './Outlay'
+import ShowData from './ShowData'
+import { useIntern } from '../../Context/PublicationContext'
 
-export default function Content() {
+function Content() {
+  const {openModel,setOpenModel}=useIntern();
   return (
-    <div>
-      Publication
+    <div className="w-full flex-col">
+      <Bar/>
+      <ShowData/>
+      {openModel && <Outlay handleOpenModel={setOpenModel}/>}
     </div>
   )
 }
+
+export default Content
