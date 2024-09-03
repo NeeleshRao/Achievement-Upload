760f0fcecee5023421ee9b87697b9f5f7942305e
S MOHAMMED ASHIQ <105161538+SMOHAMMEDASHIQ@users.noreply.github.com>
2024-09-03 23:44:52 +0530

    Update Sports.jsx

--- src/Pages/Sports/Sports.jsx
+++ src/Pages/Sports/Sports.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import { useAuth } from '../../Context/AuthContext';
-import HNavbar from '../../Components/HNavbar';
-import VNavbar from '../../Components/VNavbar';
-import Content from './Content';
 
-function Sports() {
-    const { logOut, user } = useAuth();
-    return (
-      <>
-        <HNavbar />
-        <div className="flex absolute top-[100px] left-0 right-0 bottom-0">
-          <VNavbar option="Sports" />
+import React from "react";
+import { useAuth } from "../../Context/AuthContext";
+import VNavbar from "../../Components/VNavbar";
+import HNavbar from "../../Components/HNavbar";
+import Content from "./Content";
+import { InternProvider } from "../../Context/SportsContext";
+
+function Sport() {
+  const { logOut, user } = useAuth();
+  return (
+    <>
+      <HNavbar />
+      <div className="flex absolute top-[100px] left-0 right-0 bottom-0 bg-gray-200 ">
+        <VNavbar option="Sport"/>
+        <InternProvider>
           <Content />
-        </div>
-      </>
-    );
+        </InternProvider>
+      </div>
+    </>
+  );
 }
 
-export default Sports
+export default Sport;
